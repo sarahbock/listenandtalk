152205c509de03a4afccfe59f66aef83c95c4903
Sarah Bock <mail.sarahbock@gmail.com>
2024-06-03 16:01:58 +1000

    dharug changes

--- js/index.js
+++ js/index.js
@@ -51,7 +51,9 @@ var audioError=0;
 var playbackspeed=1;
 //get local images if offline
 var imagepath= (window.navigator.onLine) ? web_dir+language+"/img/" : "img/";
-var speakers=[]; var topics=[]; var conversations=[]; var favourites=[]; var chunkbank=[]; var chunkbankSorted=[]; var chunkbankSortedLength=[]; var chunkbankFlags=[]; var entryConversation=[];
+var speakers=[]; var topics=[]; var conversations=[]; var favourites=[]; var chunkbank=[]; var chunkbankSorted=[]; var chunkbankAlphabetical=[]; var chunkbankSortedLength=[]; var chunkbankFlags=[]; var entryConversation=[];
+var topicsToHide = (language === 'dharug') ? ['27','28','37','42','43','39','38','44','45','46','47'] : [];
+//any entries with these as topics will be hidden from the filtered and full list
 var initialActivityWordLength=20;//maximum length of words for intial set up of Have a go activity
 var maxMemoryWordLength=200;//maximum length of words memory activity
 var tokenRequest=""; //link and text to point people to right place to get token
@@ -202,8 +204,18 @@ function initialiseDictionary(){
         chunkbank[a][languageCol]=removeGlossing(chunkbank[a][languageCol]);
         //remove any entries that have null for both languages
         //if (chunkbank[a].english==="" && chunkbank[a].mangarrayi===""){chunkbank=chunkbank.splice(a,1);}
+        if (language==="dharug" || language==="sandpit") {
+            chunkbank[a].displayorder = Number(chunkbank[a].displayorder)
+        }
     }
-    chunkbankSorted = [...chunkbank].sort((a,b) => (a[translationCol] > b[translationCol]) ? 1 : ((b[translationCol] > a[translationCol]) ? -1 : 0));//sort in A-Z order of english
+    chunkbankAlphabetical = [...chunkbank].sort((a,b) => (a[translationCol] > b[translationCol]) ? 1 : ((b[translationCol] > a[translationCol]) ? -1 : 0));//sort in A-Z order of english
+    if (language==="dharug" || language==="sandpit") {
+        chunkbankSorted = [...chunkbank].sort((a,b) => ( a.displayorder - b.displayorder || a[translationCol].localeCompare(b[translationCol])));//sort in display order then in A-Z order of english
+    } else {
+        chunkbankSorted = [...chunkbank].sort((a,b) => (a[translationCol] > b[translationCol]) ? 1 : ((b[translationCol] > a[translationCol]) ? -1 : 0));//sort in A-Z order of english
+    }
+    var keyArray = chunkbankSorted.map(function(item) { return {'display':item.displayorder,'english':item[translationCol]}; });
+    console.log(keyArray)
     chunkbankSortedLength=[...chunkbank].sort((a,b) => (a[translationCol].length > b[translationCol].length) ? 1 : ((b[translationCol].length > a[translationCol].length) ? -1 : 0));//sort by length of english
     getData();
     $(".preloadContainer").html(preloadHTML);
@@ -307,15 +319,16 @@ function stripHTML(html){//strip HTML tags from string
 function getData(){
     //this function is called when dictionary is initialised or when language is changed
     console.log("============GET DATA ",languageFirst);
-
     //create full list
     var listHTML = ""; var startHTML=""; var languageColHTML = ""; var translationColHTML = ""; var endHTML="";
-    for (var a=0; a<chunkbankSorted.length; a++){ //chunkbankSorted is in A-Z order of english text
-        if(chunkbankSorted[a].id!=="0" && chunkbankSorted[a][languageCol]!==""){
+    for (var a=0; a<chunkbankAlphabetical.length; a++){ //chunkbankAlphabetical is in A-Z order of english text
+        //don't show any entries from the hidden topics in the full list
+        if(chunkbankAlphabetical[a].id!=="0" && chunkbankAlphabetical[a][languageCol]!=="" && !topicsToHide.includes(chunkbankAlphabetical[a].topic)){
+            //console.log(chunkbankAlphabetical[a].topic);
             startHTML='<div class="entry">';
-            translationColHTML = '<div class="entryEnglish" onclick="setEntry(\''+chunkbankSorted[a].id+'\'); showPage(\'entry\');">'+ chunkbankSorted[a][translationCol]+'</div>';
-            languageColHTML = '<div class="entryMangarrayi audioButtonDiv active" id="fulllistaudio_'+chunkbankSorted[a].id+'" onclick="toggleAudio(\'fulllistaudio_'+chunkbankSorted[a].id+'\');"><img src="images/audio_on.png" alt="play" title="Play" class="audioIcon">'+chunkbankSorted[a][languageCol]+'</div>';
-            endHTML='<div class="entryGo active" onclick="setEntry(\''+chunkbankSorted[a].id+'\');showPage(\'entry\');"><img src="images/icon_right.png" alt="arrow right"></div><div class="clearBoth"> </div> </div>';
+            translationColHTML = '<div class="entryEnglish" onclick="setEntry(\''+chunkbankAlphabetical[a].id+'\'); showPage(\'entry\');">'+ chunkbankAlphabetical[a][translationCol]+'</div>';
+            languageColHTML = '<div class="entryMangarrayi audioButtonDiv active" id="fulllistaudio_'+chunkbankAlphabetical[a].id+'" onclick="toggleAudio(\'fulllistaudio_'+chunkbankAlphabetical[a].id+'\');"><img src="images/audio_on.png" alt="play" title="Play" class="audioIcon">'+chunkbankAlphabetical[a][languageCol]+'</div>';
+            endHTML='<div class="entryGo active" onclick="setEntry(\''+chunkbankAlphabetical[a].id+'\');showPage(\'entry\');"><img src="images/icon_right.png" alt="arrow right"></div><div class="clearBoth"> </div> </div>';
             //set column 1 to be language column if this display is prefered
             if (languageFirst){
                 listHTML+=startHTML+languageColHTML+translationColHTML+endHTML;
@@ -661,7 +674,7 @@ function searchDictionary(word){
     var results=[]; var resultEnglish=""; var resultMang="";
     var count=0; var limit=1000;//limit number of search results
 
-    var needle = word.replace(/-/g, '').trim().toLowerCase(); //remove dashes
+    var needle = word.replace(/-|,/g, '').trim().toLowerCase(); //remove dashes and commas
     //console.log("============SEARCH: "+needle);
     var length=needle.length;
     var haystack;
@@ -671,7 +684,7 @@ function searchDictionary(word){
     //search start of mangarray words (to cater for n, ny, nya- etc)
     for (var f=0; f<chunkbank.length; f++){
         if (count<limit){
-            haystack=chunkbank[f][languageCol].toLowerCase().replace(/-/g, ''); haystack=haystack.substring(0,length);
+            haystack=chunkbank[f][languageCol].toLowerCase().replace(/-|,/g, ''); haystack=haystack.substring(0,length);
             if (haystack.indexOf(needle)!==-1){
                 //console.log(haystack);
                 if ($.inArray(f,results)===-1){results.push(f);count++;}//push into results if not already in results
@@ -682,7 +695,7 @@ function searchDictionary(word){
     //search anywhere in mangarray words
     for (var k=0; k<chunkbank.length; k++){
         if (count<limit){
-            haystack=chunkbank[k][languageCol].toLowerCase().replace(/-/g, '');
+            haystack=chunkbank[k][languageCol].toLowerCase().replace(/-|,/g, '');
             if (haystack.indexOf(needle)!==-1){
                 //console.log(haystack);
                 if ($.inArray(k,results)===-1){results.push(k);count++;}//push into results if not already in results
@@ -693,7 +706,7 @@ function searchDictionary(word){
     //then search english
     for (var d=0; d<chunkbank.length; d++){
         if (count<limit){
-            haystack = chunkbank[d][translationCol].toLowerCase().replace(/-/g, ''); //haystack=haystack.substring(0,length);
+            haystack = chunkbank[d][translationCol].toLowerCase().replace(/-|,/g, ''); //haystack=haystack.substring(0,length);
             //if (chunkbank[d].id==="137"){//console.log("Tag: "+haystack+" Needle: "+needle);}
             if (haystack.indexOf(needle)!==-1){
                 if ($.inArray(d,results)===-1){results.push(d);count++;}//push into results if not already in results
@@ -707,7 +720,7 @@ function searchDictionary(word){
         if (count<limit && chunkbank[e].keyword!==null){
             var keywordarray=chunkbank[e].keyword.split(",");
             for (var km=0; km<keywordarray.length; km++){//array of separate keywords for each entry
-                haystack=keywordarray[km].toLowerCase().replace(/-/g, '').trim(); //keyword to lower case and trim
+                haystack=keywordarray[km].toLowerCase().replace(/-|,/g, '').trim(); //keyword to lower case and trim
                 haystack=haystack.substring(0,length);
                 if (needle===haystack){ if ($.inArray(e,results)===-1){ results.push(e); count++; } }
             }
@@ -718,7 +731,7 @@ function searchDictionary(word){
         if (count<limit && chunkbank[g][keywordTranslationCol]!==null){
             var keywordengarray=chunkbank[g][keywordTranslationCol].split(",");
             for (var ke=0; ke<keywordengarray.length; ke++){//array of separate keywords for each entry
-                haystack=keywordengarray[ke].toLowerCase().replace(/-/g, '').trim(); //keyword to lower case and trim
+                haystack=keywordengarray[ke].toLowerCase().replace(/-|,/g, '').trim(); //keyword to lower case and trim
                 haystack=haystack.substring(0,length);
                 if (needle===haystack){ if ($.inArray(g,results)===-1){ results.push(g); count++; } }
             }
@@ -729,7 +742,7 @@ function searchDictionary(word){
         if (count<limit  && chunkbank[t].tags!==null){
             var tagarray=chunkbank[t].tags.split(",");
             for (var tt=0; tt<tagarray.length; tt++){//array of separate tags for each entry
-                haystack=tagarray[tt].toLowerCase().replace(/-/g, '').trim(); //keyword to lower case and trim
+                haystack=tagarray[tt].toLowerCase().replace(/-|,/g, '').trim(); //keyword to lower case and trim
                 //if (chunkbank[t].id==="2"){//console.log("Tag: "+haystack+" Needle: "+needle);}
                 haystack=haystack.substring(0,length);
                 if (needle===haystack){ if ($.inArray(t,results)===-1){ results.push(t); count++; } }
@@ -742,8 +755,8 @@ function searchDictionary(word){
             var a=parseInt(results[r]);
             //console.log("============SEARCH: "+r+" needle: "+word+" haystack: "+haystack);
             //clean up haystacks
-            var english=chunkbank[a][translationCol]; resultEnglish=english; var englishHaystack=english.toLowerCase().replace(/-/g, '');
-            var mangarray=chunkbank[a][languageCol]; resultMang=mangarray; var mangarrayHaystack=mangarray.toLowerCase().replace(/-/g, '');
+            var english=chunkbank[a][translationCol]; resultEnglish=english; var englishHaystack=english.toLowerCase().replace(/-|,/g, '');
+            var mangarray=chunkbank[a][languageCol]; resultMang=mangarray; var mangarrayHaystack=mangarray.toLowerCase().replace(/-|,/g, '');
 
             //can we find search terms in the mangarray word?
             var yolS=mangarrayHaystack.indexOf(needle);
@@ -1954,21 +1967,24 @@ function loadFilterEntries(){
     $("#filterEntries").val(selectedFilter);
     var allFilterArray=[];
     for (var b=0; b<chunkbank.length; b++){
-        //get all the items for that filter e.g. all the english keywords in the dictionary
-        if (chunkbank[b][selectedFilter]==null){chunkbank[b][selectedFilter]="";}
-        var tempFilterArray = chunkbank[b][selectedFilter].split(",");
-        for (var c=0; c<tempFilterArray.length; c++){
-            var newItem = tempFilterArray[c].replace(/-/g, '').trim();//replace dashes and extra space from filter item
-            //fix spelling mistakes and duplicates
-           if (newItem==="I am"){newItem="I'm";}
-           // if (selectedFilter==="keywordenglish"){newItem=newItem.toLowerCase();}//change english items to lower case so they appear in alphabetical order
-           // if (selectedFilter==="keyword"){newItem=newItem.toLowerCase();}//change mangarrayi  items to lower case
-            //change all filter items to lower case, unless the filter word is all caps
-            newItem = (newItem === newItem?.toUpperCase()) ? newItem : newItem=newItem?.toLowerCase();
-            //push filter items into an array and avoid duplicates
-            if (allFilterArray.indexOf(newItem) === -1 && newItem!==""){
-                allFilterArray.push(newItem);
-            } 
+        //don't include any entries from the topics that are hidden from the filtered list
+        if (!topicsToHide.includes(chunkbank[b].topic)) {
+             //get all the items for that filter e.g. all the english keywords in the dictionary
+            if (chunkbank[b][selectedFilter]==null){chunkbank[b][selectedFilter]="";}
+            var tempFilterArray = chunkbank[b][selectedFilter].split(",");
+            for (var c=0; c<tempFilterArray.length; c++){
+                var newItem = tempFilterArray[c].replace(/-/g, '').trim();//replace dashes and extra space from filter item
+                //fix spelling mistakes and duplicates
+            if (newItem==="I am"){newItem="I'm";}
+            // if (selectedFilter==="keywordenglish"){newItem=newItem.toLowerCase();}//change english items to lower case so they appear in alphabetical order
+            // if (selectedFilter==="keyword"){newItem=newItem.toLowerCase();}//change mangarrayi  items to lower case
+                //change all filter items to lower case, unless the filter word is all caps
+                newItem = (newItem === newItem?.toUpperCase()) ? newItem : newItem=newItem?.toLowerCase();
+                //push filter items into an array and avoid duplicates
+                if (allFilterArray.indexOf(newItem) === -1 && newItem!==""){
+                    allFilterArray.push(newItem);
+                } 
+            }
         }
     }
     //now sort filter item list into alphabetical order, ignoring any * sign at the start
